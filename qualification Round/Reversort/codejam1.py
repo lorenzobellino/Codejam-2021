4ade29615e9c6a1e79055bfc653dca361966f873
Mazzi <lor.bellino@gmail.com>
2021-04-09 12:54:38 +0200

    added Reversort

--- qualification Round/Reversort/codejam1.py
+++ qualification Round/Reversort/codejam1.py
@@ -4,10 +4,8 @@ for i in range(T):
     iteration = 0
     N = input()
     v = [int(x) for x in input().split(" ")]
-     #print(v)
     for z in range(0,len(v)-1):
         j = v.index(min(v[z:len(v)]))
         v = v[0:z] + list(reversed(v[z:j+1])) + v[j+1:len(v)]
-        #print("{} {} {}".format( v[0:i] , list(reversed(v[i:j+1])) , v[j+1:len(v)]))
         iteration+=(j-z+1)
     print("Case #"+str(i+1)+": "+str(iteration))
